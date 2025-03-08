c82ce4045ebb56813fe4becede113be2b7de03d0
Oliden Grande <olidengrande@hotmail.com>
2025-03-08 15:17:42 -0400

    Adjust header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Header from "./Header";
 
 function App() {
   return (
-    /* <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>🚀 Oliden Grande</h1>
-      <p>Under construction.</p>
-      <a href="/politica-privacidad">Política de Privacidad</a>
-    </div> */
     <div className="App">
       <Header />
-      //Resto del código
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+          <h1>🚀 Oliden Grande</h1>
+          <p>Under construction.</p>
+          <a href="/politica-privacidad">Política de Privacidad</a>
+      </div>
     </div>
   );
 }
