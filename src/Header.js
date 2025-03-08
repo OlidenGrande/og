2f404d332635f3c1e2dc84a668bbbf5963c5e809
Oliden Grande <olidengrande@hotmail.com>
2025-03-08 15:39:58 -0400

    Style adjustments in Header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
-import logo from '../src/assets/Logo_OG.png'
+import logo from '../src/assets/Logo_OG.png';
+import './Header.css';
 
 function Header() {
     return(
-        <header style={{textAlign: 'center', padding: '20px 0'}}>
-            <img src={logo} alt="Mi-Logo" style={{maxWidth: '200px'}}/>
+        <header>
+            <img src={logo} alt="Mi-Logo"/>
         </header>
     );
 }
